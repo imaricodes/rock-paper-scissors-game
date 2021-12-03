77c11b3b1979e4fe93d09c0a3e90e2eea6915c5c
Imari Childress <imaricodes@gmail.com>
2021-12-03 18:46:10 -0500

    First commit. Basic functions created (see below).
    computerPlay () - this is computer's randomly generated play. Returns rock, paper, or scissors.
    
    playerSelection() - contains a prompt for player to type rock, paper, or scissors. Returns that value.
    
    playRound - takes computerPlay() contained by var computerSelection and playerSelection as arguments,then  compares them and returns the winner.

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+//***** computerPlay() returns random computer play *****//
+
+
+function computerPlay () {
+    
+    function randomNumberGenerator (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+        }  
+
+    const randomNumberGeneratorResult = randomNumberGenerator(1,3);
+    
+    if (randomNumberGeneratorResult === 1){
+    return "rock";
+    } else if (randomNumberGeneratorResult === 2){
+        return "paper";}
+            else if (randomNumberGeneratorResult === 3){
+                return "scissors";
+            }
+};
+
+const computerSelection = computerPlay();
+
+
+//***** playerSelection() returns player chioce.. for now randomNumber is typed
+
+const playerSelection = () => {
+    //return prompt("enter choice");
+    return "rock";
+}
+
+
+//***** playRound *****//
+
+function playRound (playerSelection, computerSelection){
+    
+    if (playerSelection === "rock" && computerSelection ==="scissors") {
+        console.log(playerSelection + " " + computerSelection + " " + "player wins")
+    } 
+    else if (playerSelection === "rock" && computerSelection ==="paper"){
+        console.log(playerSelection + " " + computerSelection + " " + "computer wins")
+    }
+
+    else if (playerSelection === "paper" && computerSelection ==="rock") {
+        console.log(playerSelection + " " + computerSelection + " " + "player wins")
+    }
+
+    else if (playerSelection === "paper" && computerSelection ==="scissors") {
+        console.log(playerSelection + " " + computerSelection + " " + "computer wins")
+    }
+    else if (playerSelection === "scissors" && computerSelection ==="rock") {
+        console.log(playerSelection + " " + computerSelection + " " + "computer wins")
+    }
+
+    else if (playerSelection === "scissors" && computerSelection ==="paper") {
+        console.log(playerSelection + " " + computerSelection + " " + "player wins")
+    }
+
+    else {console.log (playerSelection + " " + computerSelection + " " + "tie")}
+
+    
+    
+    // if (playerSelection === computerSelection) {
+    //     console.log ("tie");
+    // }
+    // else {console.log( "not a tie")};
+    //return playerSelection + computerSelection;   
+};
+
+playRound(playerSelection(), computerSelection);
+
+
+
