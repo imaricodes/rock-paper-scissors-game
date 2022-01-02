1abaa1636d7acff7558f2f00fd7cff4601aa1bb1
Imari Childress <imaricodes@gmail.com>
2022-01-02 12:25:04 -0500

    Add playRound().

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ function computerPlay (){
     return gameChoices[Math.floor(Math.random()*gameChoices.length)]
 }
 
-
+//this needs to return winner for use by a score keeping function
+function playRound(playerSelection, computerSelection){
+    console.log(score);
+    console.log(playerSelection);
+    console.log(computerSelection);
+};
 
 
