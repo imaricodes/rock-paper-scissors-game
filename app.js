9efad20c721007a757b817b1e5306f9665c48090
Imari Childress <imaricodes@gmail.com>
2022-01-02 16:39:46 -0500

    Add logic to playRound() to compare computerSelection and playerSelection and return winner

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 //returns computers pick
 function computerPlay (){
     const gameChoices = ["rock", "paper", "scissors"]
@@ -7,21 +9,33 @@ function computerPlay (){
 
 //this needs to return winner for use by a score keeping function
 function playRound(playerSelection, computerSelection){
-    console.log(score);
-    console.log(playerSelection);
-    console.log(computerSelection);
+    if (computerSelection == playerSelection){
+        return 'tie'
+    } else if ((computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "paper") ||
+        (computerSelection === "paper" && playerSelection === "rock"))
+            {
+                //console.log('player wins');
+                return 'computer wins'
+            } else {
+                //console.log("inconclusive")
+                return 'player wins'
+                };
+
+    //console.log(score);
+    
 };
 
-
-let playerChoiceButtons = document.querySelectorAll(".player-choice-btn");
 
 let playerChoiceButtons = document.querySelectorAll(".player-choice-btn");
     
 playerChoiceButtons.forEach((button) => {
     button.addEventListener('click', (e)=> {
-        playerSelection = e.target.id;
+        let playerSelection = e.target.id;
         let computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-            
+        let result = playRound(playerSelection, computerSelection);
+        console.log(computerSelection);
+        console.log(playerSelection);
+        console.log(result);     
     });
 });
